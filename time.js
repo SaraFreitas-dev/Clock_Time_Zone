379e4fece9e901b87be2ff472f389e71780ddd13
Fontes <sara-raquel.freitas-fontes@capgemini.com>
2023-11-25 12:39:26 +0100

    Added 4 different time zones

---
+++ time.js
@@ -0,0 +1,29 @@
+// Function to update time for a specific country
+function updateTime(countryId, hrsId, minId, secId, offset) {
+    let hrs = document.getElementById(hrsId);
+    let min = document.getElementById(minId);
+    let sec = document.getElementById(secId);
+
+    setInterval(() => {
+        let currentTime = new Date();
+        let localTime = currentTime.getTime();
+        let localOffset = currentTime.getTimezoneOffset() * 60000;
+        let utc = localTime + localOffset;
+        let countryTime = utc + (3600000 * offset);
+
+        let countryCurrentTime = new Date(countryTime);
+
+        hrs.innerHTML = (countryCurrentTime.getHours() < 10 ? "0" : "") + countryCurrentTime.getHours();
+        min.innerHTML = (countryCurrentTime.getMinutes() < 10 ? "0" : "") + countryCurrentTime.getMinutes();
+        sec.innerHTML = (countryCurrentTime.getSeconds() < 10 ? "0" : "") + countryCurrentTime.getSeconds();
+
+    }, 1000);
+}
+
+// Update time for each country
+updateTime("frClock", "frHrs", "frMin", "sec", 1); // Example offset for France
+updateTime("ptClock", "ptHrs", "ptMin", "sec", 0); // Example offset for Oporto
+updateTime("nwClock", "nwHrs", "nwMin", "sec", -5); // Example offset for New York
+updateTime("hkClock", "hkHrs", "hkMin", "sec", 8); // Example offset for Hong Kong
+
+
